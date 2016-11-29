823773463b97055dcfc96ffa66f8f1afda1fa314
renanmpn <renan.de.jaba@gmail.com>
2016-11-29 14:32:10 -0200

    chapter 5. Fibonacci naive

---
+++ chap5/fibonacci_naive.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+#
+# Renan Monteiro Pinto Neto 29/11/16
+
+
+def fibonacci(n):
+    assert(n >= 0), 'n must be >= 0'
+    return n if n in (0, 1) else fibonacci(n-1)+fibonacci(n-2)
+
+if __name__ == '__main__':
+    from timeit import Timer
+    t = Timer('fibonacci(8)', 'from __main__ import fibonacci')
+    print(t.timeit())
